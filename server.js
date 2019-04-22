7abea2d8ab80dc93200802883a055f78fd10beee
ApiwanaSuwannapoom <5910110529@psu.ac.th>
2019-04-22 23:35:19 +0700

    สร้าง API สำหรับขอข้อมูลหนังสือทั้งหมด

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const app = express()
+const books = require('./db')
 
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
+
+app.get('/books', (req, res) => {
+  res.json(books)
+})
 app.listen(3000, () => {
   console.log('Start server at port 3000.')
 })
