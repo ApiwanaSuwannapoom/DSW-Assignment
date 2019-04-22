dd36938e20ad14de89b3b344714fa2b0ce5d95ac
ApiwanaSuwannapoom <5910110529@psu.ac.th>
2019-04-22 23:49:54 +0700

    สร้างAPIสำหรับการสร้างหนังสือและติดตั้งbody-parser

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 const books = require('./db')
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   res.send('Hello World')
@@ -19,7 +23,7 @@ app.post('/books', (req, res) => {
     books.push(req.body)
     res.status(201).json(req.body)
   })
-  
+
 app.listen(3000, () => {
   console.log('Start server at port 3000.')
 })
