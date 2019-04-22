42866f59f727b9a4b02a146195db18c771427b69
ApiwanaSuwannapoom <5910110529@psu.ac.th>
2019-04-23 02:04:09 +0700

    Deploy function REST api ขึ้น firebase

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,76 @@
 const functions = require('firebase-functions');
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const firebase = require('firebase')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.api = functions.https.onRequest(app)
+
+const config = {
+    apiKey: "AIzaSyCQsPDJpLyJ46_yoRptWci14q04623khwQ",
+    authDomain: "assignmentdsw.firebaseapp.com",
+    databaseURL: "https://assignmentdsw.firebaseio.com",
+    projectId: "assignmentdsw",
+    storageBucket: "assignmentdsw.appspot.com",
+    messagingSenderId: "629199098727"
+  };
+firebase.initializeApp(config);
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.get('/books', (req, res) => {
+    var bookRef = firebase.database().ref('books');
+    bookRef.on('value', function(snapshot) {
+      res.send(snapshot.val());
+    });
+})
+
+app.get('/books/:id', (req, res) => {
+    var id =  req.params.id
+    firebase.database().ref('/books/' + id).once('value').then(function(snapshot) {
+        res.send(snapshot.val());
+    });
+  })
+
+app.post('/books', (req, res) => {
+    var id =  req.body.id
+     var name =  req.body.name
+     var type =  req.body.type
+    
+    firebase.database().ref('/books/' + id ).set({
+          name: name,
+          id: id,
+          type: type
+          
+        });
+    res.send("success");
+  })
+
+app.put('/books/:id', (req, res) => {
+    var id =  req.body.id
+    var name =  req.body.name
+    var type =  req.body.type
+    var postData = {
+        name: name,
+        id: id,
+        type: type
+      };
+    
+    firebase.database().ref('/books/' + id ).update(postData);
+    res.send("success");
+  })
+
+app.delete('/books/:id', (req, res) => {
+    var id =  req.params.id
+    firebase.database().ref('/books/' + id ).remove();
+    res.send("success");
+ })
+
+app.listen(3000, () => {
+  console.log('Start server at port 3000.')
+})
