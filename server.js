87ba084758d7ef7cbd7a73ca33ab5a97c944fbc9
ApiwanaSuwannapoom <5910110529@psu.ac.th>
2019-04-23 00:01:39 +0700

    สร้าง API สำหรับอัพเดตข้อมูลหนังสือตาม id

--- server.js
+++ server.js
@@ -24,6 +24,11 @@ app.post('/books', (req, res) => {
     res.status(201).json(req.body)
   })
 
+app.put('/books/:id', (req, res) => {
+    const updateIndex = books.findIndex(book => book.id === req.params.id)
+    res.json(Object.assign(books[updateIndex], req.body))
+  })
+
 app.listen(3000, () => {
   console.log('Start server at port 3000.')
 })
