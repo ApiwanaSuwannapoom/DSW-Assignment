6f7963b4f7ed162e64e6face8ef5f4beae23ec5c
ApiwanaSuwannapoom <5910110529@psu.ac.th>
2019-04-23 17:50:56 +0700

    Install Axios library

--- reactcrud/src/components/index.component.js
+++ reactcrud/src/components/index.component.js
@@ -1,11 +1,46 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import TableRow from './TableRow';
 
 export default class Index extends Component {
-    render() {
-        return (
-            <div>
-                <p>Welcome to Index Component!!</p>
-            </div>
-        )
+
+  constructor(props) {
+      super(props);
+      this.state = {business: []};
     }
-}
+    componentDidMount(){
+      axios.get('https://us-central1-assignmentdsw.cloudfunctions.net/api/books/')
+        .then(response => {
+          this.setState({ books: response.data });
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+    }
+    tabRow(){
+      return this.state.business.map(function(object, i){
+          return <TableRow obj={object} key={i} />;
+      });
+    }
+
+    render() {
+      return (
+        <div>
+          <h3 align="center">Books List</h3>
+          <table className="table table-striped" style={{ marginTop: 20 }}>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Type</th>
+                <th colSpan="2">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              { this.tabRow() }
+            </tbody>
+          </table>
+        </div>
+      );
+    }
+  }
