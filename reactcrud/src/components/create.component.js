40e03b4671d093f87fa591e5e2aba16e6c5f0d0b
ApiwanaSuwannapoom <5910110529@psu.ac.th>
2019-04-23 12:47:32 +0700

    Configure React routing

---
+++ reactcrud/src/components/create.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from './node_modules/react';
+
+export default class Create extends Component {
+    render() {
+        return (
+            <div>
+                <p>Welcome to Create Component!!</p>
+            </div>
+        )
+    }
+}
---
+++ reactcrud/src/components/edit.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from './node_modules/react';
+
+export default class Edit extends Component {
+    render() {
+        return (
+            <div>
+                <p>Welcome to Edit Component!!</p>
+            </div>
+        )
+    }
+}
